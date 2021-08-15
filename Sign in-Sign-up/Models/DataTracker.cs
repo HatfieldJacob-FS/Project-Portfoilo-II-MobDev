fd30d6051778591911d6186de4e42833476a1eb9
HatfieldJacob-FS <65465286+HatfieldJacob-FS@users.noreply.github.com>
2021-08-15 16:49:47 -0700

    Add files via upload

---
+++ Sign in-Sign-up/Models/DataTracker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+
+namespace AnyOnrCanCook.Models
+{
+    class DataTracker
+    {
+        private User _getUser = new User();
+        private string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "AnyOnrCanCook.txt");
+
+       
+
+
+
+        public bool UserCheck(string checks)
+        {
+            
+            bool invalidUser = false;
+            if (File.Exists(_fileName))
+            {
+                using (var load = new StreamReader(_fileName))
+                {
+                    if (File.ReadAllLines(_fileName).Contains(checks))
+                    {
+                        invalidUser = true;
+                    }
+                    else if (File.ReadAllLines(_fileName).Contains(checks))
+                    {
+                        invalidUser = true;
+                    }
+                }
+            }//if statement ends
+           
+            return invalidUser;
+        }
+        public void UserSave(User userData)
+        {
+            using(var save = new StreamWriter(_fileName))
+            {
+                save.WriteLine(userData.Name);
+                save.WriteLine(userData.LastName);
+                save.WriteLine(userData.UserName);
+                save.WriteLine(userData.Email);
+                save.WriteLine(userData.Password);
+
+            }
+        }
+
+        public bool Validation(User data)
+        {
+            bool verfUser = false;
+            if (File.Exists(_fileName))
+            {
+                using (var valid = new StreamReader(_fileName))
+                {
+                    if (File.ReadAllLines(_fileName).Contains(data.UserName))
+                    {
+                        if (File.ReadAllLines(_fileName).Contains(data.Password))
+                        {
+                            verfUser = true;
+                        }
+                    }
+                    
+
+                }
+            }//if statement end
+            
+
+
+
+
+            return verfUser;
+        }
+        
+
+
+
+
+
+
+    }
+}
---
+++ Sign in-Sign-up/SignIn.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AnyOnrCanCook.Models;
+
+namespace AnyOnrCanCook
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignIn : ContentPage
+    {
+        private DataTracker _datatrack = new DataTracker();
+        private User _user = new User();
+        public SignIn()
+        {
+            InitializeComponent();
+            signInButton.Clicked += SignInButton_Clicked;
+            showPassword.Toggled += ShowPassword_Toggled;
+            forgotPass.Clicked += ForgotPass_Clicked;
+            forgotUser.Clicked += ForgotUser_Clicked;
+            createAcc.Clicked += CreateAcc_Clicked;
+        }
+
+        private void CreateAcc_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void ForgotUser_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hint:", "Username", "Dismiss");
+        }
+
+        private void ForgotPass_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hint:", "Password", "Dismiss");
+        }
+
+        private void ShowPassword_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (showPassword.IsToggled == false)
+            {
+                passwordEntry.IsPassword = true;
+                
+            }
+            else if (showPassword.IsToggled == true)
+            {
+                passwordEntry.IsPassword = false;
+               
+            }
+        }
+
+        private void SignInButton_Clicked(object sender, EventArgs e)
+        {
+           
+           _user.UserName = userNameEntry.Text;
+            _user.Password = passwordEntry.Text;
+           var validUser = _datatrack.Validation(_user);
+
+            if (validUser == false)
+            {
+                DisplayAlert("Error", "Username and or Password was Incorrect", "Dismiss");
+                userNameEntry.Text = "";
+                passwordEntry.Text = "";
+            }
+            else
+            {
+                DisplayAlert("Notification", "Thank you for Signing In :)", "Dismiss");
+            }
+           
+
+        }
+    }
+}
