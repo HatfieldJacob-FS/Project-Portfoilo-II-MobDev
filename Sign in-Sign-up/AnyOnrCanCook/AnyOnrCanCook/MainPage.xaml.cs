b6aed31ef8fa5e6074657f973d411e87a53731c7
HatfieldJacob-FS <jhatfield@student.fullsail.edu>
2021-08-15 07:30:12 -0700

    sign in signup changes

---
+++ Sign in-Sign-up/AnyOnrCanCook/AnyOnrCanCook/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnyOnrCanCook
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            createAccountButton.Clicked += CreateAccountButton_Clicked;
+        }
+
+        private void CreateAccountButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
